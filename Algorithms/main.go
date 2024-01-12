d8c7892d4da769a72e1f77951b29a97e7243ee14
khanh <khanhyou2018@gmail.com>
2024-01-12 15:29:53 +0700

    add commit

--- Algorithms/main.go
+++ Algorithms/main.go
@@ -1,35 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-//"github.com/vokhanh12/DataStructures"
+// "github.com/vokhanh12/DataStructures"
 
-func main() {
-	/*
+const (
+	numberOfURLs    = 100
+	numberOfWorkers = 5
+)
 
-		fmt.Println("Hello world")
-
-		ll := DataStructures.LinkedList{}
-
-		ll.Add(DataStructures.Node{Data: 3})
-		ll.Add(DataStructures.Node{Data: 6})
-		ll.Add(DataStructures.Node{Data: 5})
-		ll.Add(DataStructures.Node{Data: 9})
-		ll.Add(DataStructures.Node{Data: 8})
-		ll.Add(DataStructures.Node{Data: 7})
-
-		ll.PrintAll()
-
-		fmt.Println("SumEven:", ll.SumEven())
-
-	*/
-
-	numbers := []int{3, 7, 8, 5, 4, 2, 6, 1}
-
-	mergeSort(numbers)
-
-	for _, value := range numbers {
-		fmt.Println("", value)
+func crawlURL(queue <-chan int, name string) {
+	for v := range queue {
+		fmt.Printf("Worker %s is crawling URL %d\n", name, v)
+		time.Sleep(time.Second)
 	}
 
+	fmt.Printf("Worker %s done and exit\n", name)
+}
+
+func startQueue() <-chan int {
+	queue := make(chan int, 10)
+
+	go func() {
+		for i := 1; i <= numberOfURLs; i++ {
+			queue <- i
+			fmt.Printf("URL %d has been enqueued\n", i)
+		}
+
+		close(queue)
+	}()
+
+	return queue
+}
+
+func main() {
+	queue := startQueue()
+
+	for i := 1; i <= numberOfWorkers; i++ {
+		go crawlURL(queue, fmt.Sprintf("%d", i))
+	}
+
+	time.Sleep(time.Minute * 5)
+}
+
+func test(numDBs int) {
+	dbChan := getDatabaseChanels(numDBs)
+	fmt.Println("Waiting for database", numDBs)
+	waitForDbs(numDBs, dbChan)
+	fmt.Println("All database all online!")
+}
+
+func waitForDbs(numDbs int, dbchan chan struct{}) {
+	for i := 0; i < numDbs; i++ {
+		<-dbchan
+	}
+}
+
+func getDatabaseChanels(numDBs int) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		for i := 0; i < numDBs; i++ {
+
+			ch <- struct{}{}
+			fmt.Println("database %v is online", i+1)
+		}
+	}()
+
+	return ch
 }
