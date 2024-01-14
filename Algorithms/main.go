38c085c5406630b181f541e4960ef6165fa17546
khanh <khanhyou2018@gmail.com>
2024-01-14 17:03:34 +0700

    understand about buffered and unbufferred dm kho vl

--- Algorithms/main.go
+++ Algorithms/main.go
@@ -2,51 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
 	"time"
 )
 
 // "github.com/vokhanh12/DataStructures"
 
-func factorial(ch chan int, quit chan int) int {
-
-	counter := 1
-
-	result := 1
-
-	for {
-
-		select {
-
-		case ch <- counter:
-			result *= counter
-			counter++
-		case <-quit:
-			fmt.Println("quit")
-			return result
-
-		}
-
-	}
-
-}
-
 func main() {
-	var wg sync.WaitGroup
-	// Tạo mảng ngẫu nhiên lớn hơn
-	arrSize := 100
-	arr := make([]int, arrSize)
-	for i := range arr {
-		arr[i] = rand.Intn(100)
-	}
 
-	fmt.Println("Mảng trước sắp xếp (mảng lớn hơn):", arr)
+	var c = make(chan int, 4)
 
-	wg.Wait()
+	go func() { fmt.Printf("got %d\n", <-c) }()
 
-	fmt.Println("Mảng sau sắp xếp:", arr)
+	c <- 1
+	c <- 2
+	c <- 3
+	c <- 4
+	c <- 4
 
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second)
 
 }
